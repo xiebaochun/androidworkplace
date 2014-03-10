1b6f8192eddc9df3280975cd17f47dc7feb92817
xiebaochun <463449055@qq.com>
2014-03-10 17:38:06 +0800

    update

---
+++ DragAndDrop/src/com/example/draganddrop/Button.java
@@ -0,0 +1,10 @@
+package com.example.draganddrop;
+
+import org.cocos2d.menus.CCMenuItemImage;
+
+public class Button extends CCMenuItemImage{
+     public Button()
+     {
+    	 super();
+     }
+}
